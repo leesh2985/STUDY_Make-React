0fcbd91d0426185a1559cff7798cee97d009d77f
Seung Lee <leesh2985@naver.com>
2023-05-31 17:36:38 +0900

    정답체크

--- bulls-and-cows/src/App.js
+++ bulls-and-cows/src/App.js
@@ -5,6 +5,7 @@ import { generateRandomNumber } from "./random";
 function App() {
   const [randomNumber] = useState(generateRandomNumber());
   const [answer, setAnswer] = useState("");
+  const [logs, setLogs] = useState([]);
 
   useEffect(() => {
     console.log(randomNumber);
@@ -45,22 +46,29 @@ function App() {
         ball: 0,
       }
     );
-    console.log(strike, ball);
+
+    if (strike === 4) {
+      alert("정답입니다.");
+      setLogs([...logs, `${answer} (축하합니다. 정답입니다.)`]);
+      return;
+    }
+
+    setLogs([...logs, `${answer} (strike: ${strike}, ball: ${ball})`]);
   };
 
   return (
     <div className="App">
       <h1>숫자 야구 게임</h1>
-      <header className="header">{randomNumber}</header>
+      <header className="header">----</header>
       <section>
         <input type="text" value={answer} onChange={handleAnswerChanged} />
         <button onClick={handleSubmit}>맞춰보기</button>
       </section>
       <h2>기록</h2>
       <ol>
-        <li>1234 (strike: 0, ball: 2)</li>
-        <li>5678 (strike: 1, ball: 2)</li>
-        <li>7869 (strike: 1, ball: 2)</li>
+        {logs.map((log, index) => {
+          return <li key={`${log}_${index}`}>{log}</li>;
+        })}
       </ol>
     </div>
   );
