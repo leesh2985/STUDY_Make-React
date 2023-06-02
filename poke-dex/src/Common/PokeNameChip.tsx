b9f984c4e7b07e7c08adabcbdc959c430bbe4f1f
Seung Lee <leesh2985@naver.com>
2023-06-02 20:18:11 +0900

    기본 칸 설정

---
+++ poke-dex/src/Common/PokeNameChip.tsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+const PokeNameChip = () => {
+  return (
+    <Chip>
+      <NumberChip>
+        <Number>001</Number>
+      </NumberChip>
+      <Text>이상해씨</Text>
+    </Chip>
+  );
+};
+
+const Chip = styled.div`
+  display: flex;
+  align-items: center;
+
+  border: 1px solid #c0c0c0;
+  border-radius: 16px;
+
+  font-weight: bold;
+  box-shadow: 0.5px 0.5px 0 0 #c0c0c0;
+`;
+
+// 번호
+const NumberChip = styled.div`
+  padding: 4px 6px;
+  background-color: yellow;
+  border-radius: 16px;
+  opacity: 0.8;
+`;
+
+const Number = styled.label`
+  opacity: 1;
+`;
+
+// 이름
+const Text = styled.label`
+  margin: 0 8px 0 5px;
+`;
+
+export default PokeNameChip;
--- poke-dex/src/List/PokeCard.tsx
+++ poke-dex/src/List/PokeCard.tsx
@@ -1,7 +1,24 @@
 import styled from "@emotion/styled";
+import PokeNameChip from "../Common/PokeNameChip";
+import PokeMarkChip from "../Common/PokeMarkChip";
+
+const TempimgUrl =
+  "https://mblogthumb-phinf.pstatic.net/20160817_259/retspe_14714118890125sC2j_PNG/%C7%C7%C4%AB%C3%F2_%281%29.png?type=w800";
 
 const PokeCard = () => {
-  return <Item></Item>;
+  return (
+    <Item>
+      <Header>
+        <PokeNameChip />
+      </Header>
+      <Body>
+        <Image src={TempimgUrl} alt="이상해씨 이미지" />
+      </Body>
+      <Footer>
+        <PokeMarkChip />
+      </Footer>
+    </Item>
+  );
 };
 
 // 네모칸
@@ -18,6 +35,30 @@ const Item = styled.li`
   box-shadow: 1px 1px 3px 1px#c0c0c0;
 `;
 
-const Header = styled.section``;
+// 포켓몬
+const Header = styled.section`
+  display: flex;
+  flex-direction: row;
+  margin: 8px 0;
+`;
+
+const Body = styled.section`
+  display: flex;
+  flex: 1 1 auto;
+  justify-content: center;
+  align-items: center;
+  margin: 8px 0;
+`;
+
+const Image = styled.img`
+  width: 180px;
+  height: 180px;
+`;
+
+const Footer = styled.section`
+  display: flex;
+  flex-direction: row;
+  margin: 8px 0;
+`;
 
 export default PokeCard;
