98ace2bf7ab9969905f146af3e1b6c8f258dae9e
Seung Lee <leesh2985@naver.com>
2023-06-02 13:21:52 +0900

    useReducer 사용

---
+++ todo-list/src/Todo/todoInputReducer.ts
@@ -0,0 +1,27 @@
+type TodoInputStateType = {
+  test: string;
+};
+
+// change, clear
+type TodoInputActionType =
+  | {
+      type: "change";
+      payload: string;
+    }
+  | {
+      type: "clear";
+    };
+
+export const todoInputReducer = (
+  state: TodoInputStateType,
+  action: TodoInputActionType
+) => {
+  switch (action.type) {
+    case "change":
+      return {
+        text: action.payload,
+      };
+    case "clear":
+      return { text: "" };
+  }
+};
