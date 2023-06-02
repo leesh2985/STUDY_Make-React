b34219255f84b9097c46f8fb4b9232e4d0a6563f
Seung Lee <leesh2985@naver.com>
2023-06-03 01:33:00 +0900

    react-redux랑 악연인듯

--- poke-dex/src/App.tsx
+++ poke-dex/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import PageHeader from "./Common/PageHeader";
 import PageNavigator from "./PageNavigator";
+import { Provider } from "react-redux";
+import { store } from "./Store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <PageHeader />
-      <PageNavigator />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PageHeader />
+        <PageNavigator />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
