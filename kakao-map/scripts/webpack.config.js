70c8b4eac8a58dfb309d364a74e80835fc4cbfde
Seung Lee <leesh2985@naver.com>
2023-06-03 12:51:02 +0900

    리액트 기본 설정

---
+++ kakao-map/scripts/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path"); // import path freom 'path'
+
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // import path freom 'path'
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+// export default
+module.exports = {
+  entry: "./src/index.jsx",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "..//build"),
+    filename: "static/js/[name].[contenthash:8].js",
+    clean: true,
+  },
+  devtool: isProduction ? false : "eval-source-map",
+  decServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    client: {
+      overlay: true,
+      progress: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            ues: {
+              loader: "babel-loader",
+            },
+          },
+          {
+            test: /\.css$/i,
+            exclude: /node_modules/,
+            use: [
+              isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+              "css-loader",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    isProduction
+      ? new HtmlWebpackPlugin({
+          template: "public/index.html",
+          minify: true,
+        })
+      : new HtmlWebpackPlugin({
+          template: "public/index.html",
+        }),
+    isProduction
+      ? new MiniCssExtractPlugin({
+          linkType: false,
+          filename: "[name].[contenthash:8].css",
+        })
+      : undefined,
+  ].filter(Boolean),
+};
